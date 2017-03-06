ffa275490eaa5dd89c8b757856924306195d2e47
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-03-06 12:17:10 +0000

    Basic sign-in form and refactor

--- controllers/account.rb
+++ controllers/account.rb
@@ -3,6 +3,7 @@ class Account < SwissCheese
   get '/sign-up' do
     erb :'account/sign-up'
   end
+
   post '/sign-up' do
     user = User.new(user_name: params['user_name'], password: params['password'])
     user = User.new(params[:user])
@@ -11,6 +12,9 @@ class Account < SwissCheese
     else
       "There was an error, please try again"
     end
+  end
 
+  get '/sign-in' do
+    erb :'account/sign-in'
   end
 end
