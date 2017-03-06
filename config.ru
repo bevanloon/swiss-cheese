4a231a0e1159d97d791e7d7f10570dfc4f915ede
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-03-06 11:38:29 +0000

    Basic sign-up

--- config.ru
+++ config.ru
@@ -2,9 +2,12 @@ require './environment'
 require './swiss_cheese'
 require './controllers/posts'
 require './controllers/search'
+require './controllers/account'
 
 require './models/post'
+require './models/user'
 
 map('/') { run SwissCheese }
 map('/posts') { run Posts }
 map('/search') { run Search }
+map('/account') { run Account }
---
+++ controllers/account.rb
@@ -0,0 +1,16 @@
+class Account < SwissCheese
+
+  get '/sign-up' do
+    erb :'account/sign-up'
+  end
+  post '/sign-up' do
+    user = User.new(user_name: params['user_name'], password: params['password'])
+    user = User.new(params[:user])
+    if user.save
+      "Your account has been created"
+    else
+      "There was an error, please try again"
+    end
+
+  end
+end
