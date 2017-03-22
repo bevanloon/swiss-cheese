078c7fb967b231cc1149e07e8dcdd147ed25c8f3
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-03-22 10:30:03 +0000

    Add redirect to home on sign in

--- controllers/account.rb
+++ controllers/account.rb
@@ -21,7 +21,7 @@ class Account < SwissCheese
     user = User.find_by(user_name: params[:user]['user_name'])
     if user && user.authenticate(params[:user]['password'])
       session[:user_id] = user.id
-      'Signed in'
+      redirect to('/')
     else
       'Uh oh'
     end
