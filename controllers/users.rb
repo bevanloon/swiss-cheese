8249a778142547ae87c63700b539e4d040cd4d06
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-04-13 19:37:14 +0100

    Add profile

--- controllers/users.rb
+++ controllers/users.rb
@@ -4,4 +4,15 @@ class Users < SwissCheese
     @users = User.all
     erb :'users/index'
   end
+
+  get '/profile' do
+    must_login!
+    @user = current_user
+    erb :'users/profile'
+  end
+
+  post '/profile' do
+    user = User.where(id: current_user.id).update_all("password = '#params[:password]'")
+    "saved"
+  end
 end
